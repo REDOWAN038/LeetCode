1e708468772c997f0bbd77c0ee0f0162244bff9a
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-02-17 08:57:39 +0600

    1642. Furthest Building You Can Reach

---
+++ 1642 Furthest Building You Can Reach.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int>pq;
+        int i;
+        for(i=0;i<heights.size()-1;i++){
+            int diff = heights[i+1]-heights[i];
+            if(diff<=0){
+                continue;
+            }
+
+            bricks-=diff;
+            pq.push(diff);
+
+            if(bricks<0){
+                ladders-=1;
+                bricks+=pq.top();
+                pq.pop();
+            }
+
+            if(ladders<0){
+                break;
+            }
+        }
+
+        return i;
+    }
+};
