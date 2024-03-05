6da2f1b6d8ccdeff805ea3120a8eb8e08c3d8801
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-03-05 09:11:18 +0600

    1750. Minimum Length of String After Deleting Similar Ends

---
+++ 1750 Minimum Length of String After Deleting Similar Ends.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int l=0,r=s.size()-1;
+        while(l<r && s[l]==s[r]){
+            char curr = s[l];
+
+            while(l<=r && s[l]==curr){
+                l++;
+            }
+
+            while(l<=r && s[r]==curr){
+                r--;
+            }
+        }
+
+        return r-l+1;
+    }
+};
