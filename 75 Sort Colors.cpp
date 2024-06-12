a783eb32b67e16b246c0d292bb6b99d8cd7eca1a
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-06-12 11:43:06 +0600

    75. Sort Colors

---
+++ 75 Sort Colors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int r=0,w=0,b=nums.size()-1;
+        while(w<=b){
+            if(nums[w]==0){
+                swap(nums[w++],nums[r++]);
+            }else if(nums[w]==1){
+                w++;
+            }else{
+                swap(nums[w],nums[b--]);
+            }
+        }
+    }
+};
