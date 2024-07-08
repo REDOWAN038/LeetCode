bcba180e3cc0011c6e0c111788f511142e8a7f84
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-07-08 14:22:11 +0600

    81. Search in Rotated Sorted Array II

---
+++ 81 Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0,h=n-1;
+
+        while(l<=h){
+            int m = (l+h)/2;
+
+            if(nums[m]==target){
+                return true;
+            }
+
+            if(nums[l]==nums[m] && nums[h]==nums[m]){
+                l+=1;
+                h-=1;
+                continue;
+            }
+
+            if(nums[l]<=nums[m]){
+                if(target>=nums[l]&&target<=nums[m]){
+                    h=m-1;
+                }else{
+                    l=m+1;
+                }
+            }
+
+            else{
+                if(nums[m]<=target && target<=nums[h]){
+                    l=m+1;
+                }else{
+                    h=m-1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
